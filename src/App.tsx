69179892a8ccb8789710b7ddee4fde521ad7f8c6
Aaron.ZhiRen.Shey <aaron.z.shey@proton.me>
2024-12-29 03:02:50 -0800

    update cmds

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import { useRef } from 'react'
 import { Mesh } from 'three'
 import type { DataTexture } from 'three'
 import { Canvas, useLoader, useFrame } from '@react-three/fiber'
