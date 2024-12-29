8479a382aaeaa88db81ec3e20ade99a6b8cbffc6
Aaron.ZhiRen.Shey <aaron.z.shey@proton.me>
2024-12-28 16:15:48 -0800

    figured out scrolls, made mor eprogress

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,19 @@ import { Mesh } from 'three'
 import type { DataTexture } from 'three'
 import { Canvas, useLoader, useFrame } from '@react-three/fiber'
 import {
-    Caustics,
     useGLTF,
-    CubeCamera,
     Environment,
-    OrbitControls,
     MeshRefractionMaterial,
+    useScroll,
+    ScrollControls,
+    Text3D,
+    MeshDistortMaterial
 } from '@react-three/drei'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import { RGBELoader } from 'three-stdlib'
 
-import { easing } from 'maath'
-
-
 import type { MeshProps } from '@react-three/fiber'
+import * as THREE from 'three'
 
 function Diamond(props: MeshProps) {
     const ref = useRef<Mesh>(null!)
@@ -55,15 +54,91 @@ function BuildEnvAndLight() {
         </>
     );
 }
+
+
+
+function MoveCameraOnScroll() {
+    const scroll = useScroll()
+    useFrame((state) => {
+        const offset = 1 - scroll.offset
+        state.camera.position.y = THREE.MathUtils.lerp(state.camera.position.y, (offset * 10) - 10, 0.1)
+        console.log(state.camera.position)
+        //state.camera.lookAt(0, 0, 0)
+    })
+    return <></>
+}
+
+function AboutMe() {
+    return (
+        <Text3D
+            curveSegments={32}
+            bevelEnabled
+            bevelSize={0.04}
+            bevelThickness={0.01}
+            height={0.1}
+            lineHeight={0.5}
+            letterSpacing={0.06}
+            size={0.6}
+            position={[-6, 3, -1]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            font="/Inter_Thin.json">
+
+            {`Scroll down`}
+
+            <MeshDistortMaterial
+                distort={0.01}
+                transmission={0}
+                thickness={-0.5}
+                roughness={0}
+                iridescence={1}
+                iridescenceIOR={1}
+                iridescenceThicknessRange={[0, 1200]}
+                clearcoat={1}
+                clearcoatRoughness={0}
+                envMapIntensity={1.5}
+                color={"white"}
+            />
+        </Text3D>
+    )
+}
+
+function RealAboutMe() {
+    return (
+        <Text3D
+            curveSegments={32}
+
+            height={0.1}
+            lineHeight={1}
+            letterSpacing={0.01}
+            size={0.1}
+            position={[-1, -0.6, 0]}
+            rotation={[0, 0, 0]}
+            font="/Inter_Thin.json">
+
+            {`I'm Aaron Shey\n
+            I write websites and tutorials for fun.\n
+            Click on the bubbles to learn more.\n
+            `}
+
+            <meshNormalMaterial />
+
+        </Text3D>
+    )
+}
+
 export default function App() {
     return (
-        <Canvas shadows camera={{ position: [-5, 0.5, 5], fov: 45 }}>
-            <Diamond position={[0, -0.175 + 0.5, 0]} />
-            <BuildEnvAndLight />
-            <EffectComposer>
-                <Bloom luminanceThreshold={1} intensity={2} levels={9} mipmapBlur />
-            </EffectComposer>
-            <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} minAzimuthAngle={-Math.PI / 2} maxAzimuthAngle={Math.PI / 2} />
-        </Canvas>
+        <Canvas shadows camera={{ fov: 45, position: [0, 0, 5] }}>
+            <ScrollControls>
+                <MoveCameraOnScroll />
+                <Diamond />
+                <AboutMe />
+                <RealAboutMe />
+                <BuildEnvAndLight />
+                <EffectComposer>
+                    <Bloom luminanceThreshold={1} intensity={2} levels={9} mipmapBlur />
+                </EffectComposer>
+            </ScrollControls>
+        </Canvas >
     )
 }
